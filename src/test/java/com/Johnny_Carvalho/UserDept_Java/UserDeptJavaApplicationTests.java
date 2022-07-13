59de0b9714c4cbf04feabc8ff5b10e04e9dd729b
johnnycarvalhodev <johnnycarvalhodev@gmail.com>
2022-07-12 21:24:07 -0300

    commit

---
+++ src/test/java/com/Johnny_Carvalho/UserDept_Java/UserDeptJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Johnny_Carvalho.UserDept_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDeptJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
